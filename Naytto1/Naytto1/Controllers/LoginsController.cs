a4aeac5bd0537a60d0e7d5ce6794306e546f189e
Timo Tirkkonen <timo.tirkkonen@student.careeria.fi>
2020-02-26 01:44:47 +0200

    Logins formit

---
+++ Naytto1/Naytto1/Controllers/LoginsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Naytto1.Controllers
+{
+    public class LoginsController : Controller
+    {
+        // GET: Logins
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
